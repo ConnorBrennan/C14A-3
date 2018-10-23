bed584273a36eb7a8995bf31d61df0a412af7f26
Connor M Brennan <cmbrennan@mymail.mines.edu>
2018-10-22 23:55:40 -0600

    Changed Except you're welcome back to Well, what can I say? and amazing back to awesome.

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -2,13 +2,13 @@
 public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
-			System.out.println("You're amazing");
+			System.out.println("You're awesome");
 		else if (score > 90)
 			System.out.println("That's great");
 		else if (score > 60)
 			System.out.println("That's good");
 		else
-			System.out.println("Except you're welcome");
+			System.out.println("Well, what can I say?");
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
